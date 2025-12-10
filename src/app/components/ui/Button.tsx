e13749d11c47b97b4401be13687c6d30701509ad
George Kartavenko <SocialImp@mail.ru>
2025-12-10 10:21:24 +0300

    :create: new type Button

---
+++ src/app/components/ui/Button.tsx
@@ -0,0 +1,15 @@
+type ButtonProps = {
+  children: React.ReactNode;
+  onClick?: () => void;
+};
+
+export default function Button({ children, onClick }: ButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className="w-full flex items-center justify-center gap-2 bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-700 transition"
+    >
+      {children}
+    </button>
+  );
+}
